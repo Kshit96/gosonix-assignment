7d246b268a03673ef9b23db6a4de1d2041272bf7
Kshit96 <kshit96@gmail.com>
2019-03-11 22:59:50 +0530

    minor changes

--- index.php
+++ index.php
@@ -74,7 +74,8 @@
 			echo "<br>";
 
 			if ($count>0){
-				//$visit=$row1[1]+1;
+				$visit++;
+				echo "Incremented Visit:".$Visit;
 				//$update_query="Update ip_addresses Set Visit=",$visit," where IP='".$ip2."'";
 				//$result5=$conn->query($update_query);
 				//print_r($result5->fetch_assoc());
