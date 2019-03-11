cb18e47706ca282fb0da8f4dbdb0ddf7f7b26f12
Kshit96 <kshit96@gmail.com>
2019-03-11 17:40:26 +0530

    minor changes

--- index.php
+++ index.php
@@ -13,30 +13,7 @@
 			$cleardb_username = $cleardb_url["user"];
 			$cleardb_password = $cleardb_url["pass"];
 			$cleardb_db       = substr($cleardb_url["path"],1);
-			$active_group = 'default';
-			$query_builder = TRUE;
-
-			$db['default'] = array(
-    				'dsn'    => '',
-    				'hostname' => $cleardb_server,
-    				'username' => $cleardb_username,
-   				'password' => $cleardb_password,
-    				'database' => $cleardb_db,
-    				'dbdriver' => 'mysqli',
-    				'dbprefix' => '',
-    				'pconnect' => FALSE,
-    				'db_debug' => (ENVIRONMENT !== 'production'),
-    				'cache_on' => FALSE,
-    				'cachedir' => '',
-    				'char_set' => 'utf8',
-    				'dbcollat' => 'utf8_general_ci',
-    				'swap_pre' => '',
-    				'encrypt' => FALSE,
-    				'compress' => FALSE,
-    				'stricton' => FALSE,
-    				'failover' => array(),
-    				'save_queries' => TRUE
-			);
+			
 			$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 			$table='ip_address';
 			if ($conn->connect_error) {
