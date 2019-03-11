92f3d2c77c1e7c104d661384ff4c112163002ff5
Kshit96 <kshit96@gmail.com>
2019-03-11 21:49:21 +0530

    mdebuging

--- index.php
+++ index.php
@@ -59,16 +59,7 @@
 			$result3=$conn->query($sql3);
 			$row1=mysqli_fetch_row($result3);
 
-			if ($count>0){
-				$visit=$row1[1]+1;
-				$update_query="Update ip_addresses Set Visit=",$visit," where IP='".$ip2."'";
-				$result5=$conn->query($update_query);
-				print_r($result5->fetch_assoc());
-			}else {
-				$sql4="INSERT INTO `ip_addresses` (Visit,IP) values (1,'".$ip2."')";
-				$result4=$conn->query($sql4);
-				print_r($result4->fetch_assoc());
-			}
+
 						
 			$res = mysqli_query("DESCRIBE ip_address");
 			while($row = mysqli_fetch_array($res)) {
