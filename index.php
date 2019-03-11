84b6ca65937f3b94a1ff45a319261b6bf494eac9
Kshit96 <kshit96@gmail.com>
2019-03-11 22:20:25 +0530

    minor change

--- index.php
+++ index.php
@@ -60,7 +60,7 @@
 				settype($visit,'integer');
 				$visit=$row1[1]+1;
 				echo $visit;
-				$update_query="Update `ip_addresses` Set Visit='",$visit,"' where IP='".$ip2."'";
+				//$update_query="Update `ip_addresses` Set Visit='",$visit,"' where IP='".$ip2."'";
 				//$result5=$conn->query($update_query);
 				//print_r($result5->fetch_assoc());
 			}else {
