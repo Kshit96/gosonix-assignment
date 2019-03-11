4d056693d4ff59ef5e756e2d002f6b9452ff5275
Kshit96 <kshit96@gmail.com>
2019-03-11 19:07:58 +0530

    removed all code, putting it back in step by step

--- index.php
+++ index.php
@@ -45,67 +45,22 @@
 			{
 				echo "Connected Successfully";
 			}
-			$showTable="show tables";
-			while($table1=mysqli_fetch_array($showTable))
-			{
-				echo($table1[0] . "<BR>");
-			}
-
-
 			$table='ip_address';
-			$val=mysqli_query("select 1 from `ip_address` LIMIT 1");
-			$res = mysqli_query("DESCRIBE ip_address");
-			while($row = mysqli_fetch_array($res)) {
-    				echo "{$row['Field']} - {$row['Type']}\n";
-			}
-			if($val !== FALSE)
-			{
-  				echo "Table found";
-			}
-			else
-			{
-    				echo "Table not found";
-			}
-			
-            		$sql = "CREATE TABLE IF NOT EXISTS $table (
+			$sql = "CREATE TABLE IF NOT EXISTS $table (
 				ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 				Visit INT,
 				IP VARCHAR(255)
 				)";
-
-            		$result=mysqli_query($conn, $sql);
+			$result=mysqli_query($conn, $sql);
 			echo $result,"<br>";
+
+			$sql2="INSERT INTO `ip_address` (Visit,IP) values (1,'1.1.1.1'),(1,'1.1.1.2'),(1,'1.1.1.3')";
 			
-			if ($conn->query($sql) === TRUE) {
-               			echo "Table created successfully","<br>";
+			if ($conn->query($sql2)) {
+               			echo "New record created successfully";
             		} else {
-               			echo "Error: " . $sql . "" . mysqli_error($conn),"<br>";
-            		}
-
-			$sql1="Select * from `ip_address` where IP='".$_SERVER['REMOTE_ADDR']."'";
-			$result1=$conn->query($sql1);
-			while ($row=$result1->fetch_assoc())
-			{
-				print_r($row);
-			}
-			if ($result1->num_row===0){
-				$sql2="INSERT INTO `ip_address` (Visit,IP) values (1,'".$_SERVER['REMOTE_ADDR']."')";
-				if ($conn->query($sql2)) {
-               				echo "New record created successfully";
-            			} else {
-               				echo "Error: " . $sql2 . "" . mysqli_error($conn);
-           			}
-				echo "New Row code executed";
-			}else {
-
-				echo "Add code to give unique visitor number and visitor number";
-			}
-			
-
-			//if(is_resource($result) && mysqli_num_rows($result)==1){
-			//	$row=mysqli_fetch_assoc($result);
-			//	echo $row,"<br>";
-
+               			echo "Error: " . $sql2 . "" . mysqli_error($conn);
+           		}
 
             		$conn->close();
 			echo $_SERVER['REMOTE_ADDR'];
